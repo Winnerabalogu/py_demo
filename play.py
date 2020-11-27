69f688a970636ff4899f3e5db2b99934fa01460d
Winnerabalogu <wabalogu.gmail.com>
2020-11-27 06:38:02 +0100

    fifth commit

---
+++ play.py
@@ -0,0 +1,18 @@
+import turtle
+from turtle import forward
+
+pen = turtle.Turtle()
+
+pen.color("red", "grey")
+
+for i in range(150):
+    pen.forward(100)
+    pen.left(230)
+    pen.forward(100)
+    pen.right(195)
+    pen.forward(100)
+    pen.left(230)
+    pen.forward(100)
+    pen.right(195)
+turtle.done()
+    
--- try_except.py
+++ try_except.py
@@ -21,3 +21,19 @@
 #     print("I can't kill my self")
 
 
+# try:
+#     open('new_file.txt')
+    
+# except:
+#     new_file = open('new_file.txt', 'w')
+#     new_file.write('This is a new file')
+
+try:
+    new_user = input("Do you want to open a new file y/n?: ")
+    if new_user == "y":
+        new_user = open('new_user.txt', 'w')
+        new_user.write('')
+        new_user.close()
+    else:
+        old_user = input('Enter your already existing file name: ')
+        open(old_user)
